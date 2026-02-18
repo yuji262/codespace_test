3e9c18d0c225cbad2861803d4efb976c0c586c0a
yuji262 <k809199@kansai-u.ac.jp>
2026-02-18 10:09:07 +0900

    Correct syntax and logic in test.cpp
    Fix syntax errors and improve the logic in the main function.

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+  double n = 7
+  for (int i = 0; i < 20; i++){
+    n += 2;
+    if (i % 3 = 0) {
+      n /= 1.5;
+    }
+  }
+
+  std::cout << n << std::endl;
+  
+  return 0;
+}
